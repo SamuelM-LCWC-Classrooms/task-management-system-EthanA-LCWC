52005c4deb0d42b559a8cdc9757c343c1b289c02
EthanA-LCWC <144792227+EthanA-LCWC@users.noreply.github.com>
2024-12-10 09:24:59 +0000

    Just pushing to see what I still need to do

---
+++ task_manager.py
@@ -0,0 +1,39 @@
+class TaskManager:
+    def __init__(self) -> None:
+        self.tasks = []
+    def add_task(self, title, description, priority):
+        self.status = "Incomplete"
+        self.tasks.append({
+            "title": title,
+            "description": description,
+            "priority": priority,
+            "status": self.status
+        })
+    def remove_task(self, title_to_find):
+        found_task = False
+        for task in self.tasks:
+            if task["title"] == title_to_find:
+                self.tasks.remove(task)
+                found_task = True
+                break
+        if found_task == False:
+            print("AAAAA")
+            print("ERROR: This task does not exist")
+    def mark_complete(self, title_to_find):
+        number_of_task = 0
+        for task in self.tasks:
+            if task["title"] == title_to_find:
+                new_task = task
+                new_task["status"] = "Complete"
+                self.tasks[number_of_task] = new_task
+                found_task = True
+                break
+            number_of_task += 1
+        if found_task == False:
+            print("ERROR: This task does not exist")
+    def view_tasks(self):
+        for task in self.tasks:
+            the_title = task["title"]
+            the_priority = task["priority"]
+            the_status = task["status"]
+            print(f"{the_title} (Priority: {the_priority}, Status: {the_status})")
